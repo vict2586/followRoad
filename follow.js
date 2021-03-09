c9449a2ce2ed423a1152544d6ec3d45c67df7318
Victoria <vigi2586@stud.kea.dk>
2021-03-09 16:21:39 +0100

    Done

---
+++ follow.js
@@ -0,0 +1,30 @@
+window.addEventListener("DOMContentLoaded", loadSVG);
+
+function loadSVG() {
+    console.log("load the SVG");
+
+    fetch("car_and_road.svg")
+    .then( response => response.text() )
+    .then( svgData => {
+        console.log("SVG loaded");
+
+        // TODO: put the SVG into the DOM
+
+        document.querySelector("#thesvg").innerHTML = svgData;
+
+        // TODO: Start the animation
+
+        document.querySelector("#car").classList.add("animate_car");
+
+    })
+}
+
+let car = null;
+let curve = null;
+let currentPosition;
+
+function runAnimation() {
+    console.log("animate");
+
+    // TODO: Build animation ...
+}
